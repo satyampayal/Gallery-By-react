6a16e7e3f99a56f1f419d92f7de85c6b9d61d4a5
satyam <satyampayal10@gmail.com>
2023-08-22 22:05:57 +0530

    Gallery App Inilise

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "./Components/Head";
+function App(){
+  return (
+    <>
+    <Head/>
+    </>
+  )
+
+}
+
+export default App;
---
+++ src/Components/Head.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+function Head(){
+          return(
+            <>
+                <h1  className="w-[100%] h-[200px] text-center text-[24px] tracking-[4px]"> Canva Gallery</h1>
+          </>
+          )
+}
+
+export default Head;
